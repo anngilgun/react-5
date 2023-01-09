e52d75174b1d68988f8236c18ea111acae5328ad
anngilgun <annfox41@gmail.com>
2023-01-09 23:17:44 +0200

    boostrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
-export default function App()  {
+export default function App() {
   return (
     <div className="App">
-      
-      <a
-            href="https://incandescent-stroopwafel-00eef4.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hosted on Netlify
-          </a>
+      <div className="container">
+        <a
+          href="https://incandescent-stroopwafel-00eef4.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </div>
     </div>
   );
 }
-
-export default App;
