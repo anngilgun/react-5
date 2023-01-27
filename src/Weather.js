259a717341192a76e0e6584dcd80c4415526713e
Orysia <orysiyac@varyence.com>
2023-01-27 10:58:13 +0200

    Duplicate api calls fix, some refactoring

--- src/Weather.js
+++ src/Weather.js
@@ -1,38 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./Weather.css";
 import axios from "axios";
 import WeatherInfo from "./Weatherinfo";
 import WeatherForecast from "./WeatherForecast";
-export default function Weather(props) {
+
+export default function Weather({defaultCity}) {
   const [weatherData, setWeatherData] = useState({ ready: false });
-  const [city, setCity] = useState(props.defaultCity);
-  function handleResponse(response) {
-    setWeatherData({
-      ready: true,
-      temperature: response.data.main.temp,
-      humidity: response.data.main.humidity,
-      coordinates: response.data.coord,
-      description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon,
-      wind: response.data.wind.speed,
-      city: response.data.name,
-    });
+  const [city, setCity] = useState(defaultCity);
+
+
+  const handleError = (message) => {
+    alert(message || "Oops.. Something went wrong, please try again later")
   }
+
+  const handleResponse =(response) => {
+    if (response) {
+      setWeatherData({
+        ready: true,
+        temperature: response.data.main.temp,
+        humidity: response.data.main.humidity,
+        coordinates: response.data.coord,
+        description: response.data.weather[0].description,
+        icon: response.data.weather[0].icon,
+        wind: response.data.wind.speed,
+        city: response.data.name,
+      });
+    }
+  }
+
+  const fetchInitialWeatherData = useCallback(() => {
+    let apiKey = "f5029b784306910c19746e40c14d6cd3";
+    let city = "New York";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(`${url}`).then(handleResponse).catch(handleError);
+  }, [])
+
+  useEffect(() => {
+    fetchInitialWeatherData()
+  }, [fetchInitialWeatherData])
 
   function handleSubmit(event) {
     event.preventDefault();
     search();
   }
 
-  function handleCityChange(event) {
-    setCity(event.target.value);
-  }
   function search() {
     let apiKey = "f5029b784306910c19746e40c14d6cd3";
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(url).then(handleResponse);
+    axios.get(url).then(handleResponse).catch( () => handleError("Oops.. No city found, please try again") );
   }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
@@ -52,10 +74,6 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    let apiKey = "f5029b784306910c19746e40c14d6cd3";
-    let city = "New York";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(`${url}`).then(handleResponse);
     return "Loading...";
   }
 }
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,46 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastday from "./WeatherForecastday";
-export default function WeatherForecast(props) {
-  let [loaded, setLoaded] = useState(false);
-  let [forecast, setForecast] = useState(null);
-  useEffect(() => {
-    setLoaded(false);
-  }, [props.coordinates]);
 
-  function handleResponse(response) {
-    setForecast(response.data.daily);
-    setLoaded(true);
-  }
-  function load() {
-    let apiKey = "f5029b784306910c19746e40c14d6cd3";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-  }
-  if (loaded) {
+export default function WeatherForecast({coordinates}) {
+    let [forecast, setForecast] = useState([]);
+
+    const handleResponse = (response) => {
+        if (response) {
+            setForecast(response.data.daily);
+        }
+    }
+
+    const load = useCallback( () => {
+        let apiKey = "f5029b784306910c19746e40c14d6cd3";
+        let longitude = coordinates.lon;
+        let latitude = coordinates.lat;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse).catch(() => {
+            alert("Something went wrong with fetching weather. Please try again later")
+        });
+    }, [coordinates.lon, coordinates.lat])
+
+    useEffect(() => {
+        load()
+    }, [load]);
+
     return (
-      <div className="WeatherForecast">
-        <div className="row">
-          {forecast.map(function (dailyForecast, index) {
-            if (index < 5) {
-              return (
-                <div className="col" key={index}>
-                  <WeatherForecastday data={dailyForecast} />
-                </div>
-              );
-            } else {
-              return null;
-            }
-          })}
+        <div className="WeatherForecast">
+            <div className="row">
+                {forecast.map(function (dailyForecast, index) {
+                    if (index < 5) {
+                        return (
+                            <div className="col" key={index}>
+                                <WeatherForecastday data={dailyForecast}/>
+                            </div>
+                        );
+                    } else {
+                        return null;
+                    }
+                })}
+            </div>
         </div>
-      </div>
     );
-  } else {
-    load();
-
-    return null;
-  }
 }
